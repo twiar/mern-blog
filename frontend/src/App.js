e0806deb15c95f6370619716602f91074f15e178
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-07-11 21:55:41 +0300

    login data saves in local storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
-import Container from '@mui/material/Container';
+import { Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Container from "@mui/material/Container";
 
-import { Header } from './components';
-import { Home, FullPost, Registration, AddPost, Login } from './pages';
+import { Header } from "./components";
+import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import { fetchAuthMe, selectIsAuth } from "./redux/slices/auth";
+import React from "react";
 
 function App() {
+	const dispatch = useDispatch();
+	const isAuth = useSelector(selectIsAuth);
+
+	React.useEffect(() => {
+		dispatch(fetchAuthMe());
+	}, []);
+
 	return (
 		<>
 			<Header />
-			<Container maxWidth='lg'>
+			<Container maxWidth="lg">
 				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/posts/:id' element={<FullPost />} />
-					<Route path='/add-post' element={<AddPost />} />
-					<Route path='/login' element={<Login />} />
-					<Route path='/register' element={<Registration />} />
+					<Route path="/" element={<Home />} />
+					<Route path="/posts/:id" element={<FullPost />} />
+					<Route path="/add-post" element={<AddPost />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Registration />} />
 				</Routes>
 			</Container>
 		</>
--- frontend/src/redux/slices/auth.js
+++ frontend/src/redux/slices/auth.js
@@ -1,8 +1,13 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../../axios";
 
-export const fetchAuth = createAsyncThunk("auth/fectUserData", async (params) => {
+export const fetchAuth = createAsyncThunk("auth/fetchAuth", async (params) => {
 	const { data } = await axios.post("/auth/login", params);
+	return data;
+});
+
+export const fetchAuthMe = createAsyncThunk("auth/fetchAuthMe", async () => {
+	const { data } = await axios.get("/auth/me");
 	return data;
 });
 
@@ -14,6 +19,11 @@ const initialState = {
 const authSlice = createSlice({
 	name: "auth",
 	initialState,
+	reducers: {
+		logout: (state) => {
+			state.data = null;
+		},
+	},
 	extraReducers: {
 		[fetchAuth.pending]: (state) => {
 			state.status = "loading";
@@ -27,7 +37,23 @@ const authSlice = createSlice({
 			state.status = "error";
 			state.data = null;
 		},
+		[fetchAuthMe.pending]: (state) => {
+			state.status = "loading";
+			state.data = null;
+		},
+		[fetchAuthMe.fulfilled]: (state, action) => {
+			state.status = "loaded";
+			state.data = action.payload;
+		},
+		[fetchAuthMe.rejected]: (state) => {
+			state.status = "error";
+			state.data = null;
+		},
 	},
 });
 
+export const selectIsAuth = (state) => Boolean(state.auth.data);
+
 export const authReducer = authSlice.reducer;
+
+export const { logout } = authSlice.actions;
