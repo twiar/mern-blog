0e50a654f9a03a867fb6e127658b89c0ce19d9ed
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-07-13 18:54:15 +0300

    deploy to heroku

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,25 @@
-import express from 'express';
-import multer from 'multer';
-import cors from 'cors';
+import express from "express";
+import multer from "multer";
+import cors from "cors";
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
+import { registerValidation, loginValidation, postCreateValidation } from "./validations.js";
 
-import { handleValidationErrors, checkAuth } from './utils/index.js';
+import { handleValidationErrors, checkAuth } from "./utils/index.js";
 
-import { UserController, PostController } from './controllers/index.js';
+import { UserController, PostController } from "./controllers/index.js";
 
 mongoose
-	.connect('mongodb+srv://admin:wwwwww@cluster0.1gl6v.mongodb.net/blog?retryWrites=true&w=majority')
-	.then(() => console.log('DB ok'))
-	.catch((err) => console.log('DB error', err));
+	.connect(process.env.MONGODB_URI)
+	.then(() => console.log("DB ok"))
+	.catch((err) => console.log("DB error", err));
 
 const app = express();
 
 const storage = multer.diskStorage({
 	destination: (_, __, cb) => {
-		cb(null, 'uploads');
+		cb(null, "uploads");
 	},
 	filename: (_, file, cb) => {
 		cb(null, file.originalname);
@@ -30,31 +30,31 @@ const upload = multer({ storage });
 
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
-app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
-app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
-app.get('/auth/me', checkAuth, UserController.getMe);
+app.post("/auth/login", loginValidation, handleValidationErrors, UserController.login);
+app.post("/auth/register", registerValidation, handleValidationErrors, UserController.register);
+app.get("/auth/me", checkAuth, UserController.getMe);
 
-app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
 	res.json({
 		url: `/uploads/${req.file.originalname}`,
 	});
 });
 
-app.get('/tags', PostController.getLastTags);
+app.get("/tags", PostController.getLastTags);
 
-app.get('/posts', PostController.getAll);
-app.get('/posts/tags', PostController.getLastTags);
-app.get('/posts/:id', PostController.getOne);
-app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
-app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update);
+app.get("/posts", PostController.getAll);
+app.get("/posts/tags", PostController.getLastTags);
+app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch("/posts/:id", checkAuth, postCreateValidation, PostController.update);
 
-app.listen(4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
 	if (err) {
 		return console.log(err);
 	}
 
-	console.log('Server OK');
+	console.log("Server OK");
 });
